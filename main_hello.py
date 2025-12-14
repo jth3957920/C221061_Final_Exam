ffcbd89b160baa7dc79a84f5c03ab2e8da074080
jth3957920 <64309025+jth3957920@users.noreply.github.com>
2025-12-14 11:55:32 +0900

    Implement sidebar with various widgets
    Added sidebar widgets for user input.

--- main_hello.py
+++ main_hello.py
@@ -11,3 +11,10 @@ st.set_page_config(
         "About": "전태환 - Hello Streamlit 앱입니다."
     }
 )
+st.sidebar.title('다양한 사이드바 위젯들')
+st.sidebar.checkbox('외국인 포함')
+st.sidebar.checkbox('고령인구 포함')
+st.sidebar.divider()
+st.sidebar.radio('데이터 타입', ['전체','남성','여성'])
+st.slidebar.slider('나이 선택', 0, 100, (20, 50) )
+st.slidebar.selectbox('지역 선택', ['서울','경기','인천','대전','대구','부산','광주'])
