1ae173320dd5ed7311baa1691d07f68278ef7e00
jth3957920 <64309025+jth3957920@users.noreply.github.com>
2025-12-14 11:51:23 +0900

    Update main_hello.py

--- main_hello.py
+++ main_hello.py
@@ -1,15 +1,13 @@
 import streamlit as st
 
-st.set_page_config( 
+st.set_page_config(
     page_title="Hello Streamlit",
     page_icon="👋",
     layout="wide",
-
     initial_sidebar_state="expanded",
-
     menu_items={
-        'Get Help': 'https://www.streamlit.io/help',
-        'Report a bug': "https ://streamlit.io/bug",
-        "About" :"전태환 - Hello Streamlit 앱입니다."
+        "Get Help": "https://www.streamlit.io/help",
+        "Report a bug": "https://streamlit.io/bug",
+        "About": "전태환 - Hello Streamlit 앱입니다."
     }
 )
